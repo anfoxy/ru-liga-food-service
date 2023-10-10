eb6f9e56df20895a14f1fd5f8d80d9eeaa1f61e2
anfoxy <a.kazhaeva7@mail.ru>
2023-10-10 14:49:26 +0300

    add OrderController

---
+++ order_service/src/main/java/ru/liga/order_service/controller/OrderController.java
@@ -0,0 +1,13 @@
+package ru.liga.order_service.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Api для работы с заказами")
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+
+}
